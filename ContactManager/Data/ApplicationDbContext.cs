7e303d39b2efb779b6b0c52ec5d724a349d260d2
duypham-fptaptech <duypdth2009006@fpt.edu.vn>
2022-02-20 17:47:34 +0700

    ContactManager

---
+++ ContactManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public object Contact { get; internal set; }
+    }
+}
